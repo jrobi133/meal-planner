1fdf7920df53eef25063a4a591ec4c3298917216
John Robinson <johnarobinson133@gmail.com>
2021-07-20 11:46:07 -0600

    cleaned up react starter files and starting on crud application

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Crud Application</h1>
+      <div className="form">
+        <label>Demo 1:</label>
+        <input type="text" name="demoOne" />
+        <label>Demo 2:</label>
+        <input type="text" name="demoTwo" />
+
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -5,4 +5,8 @@ const app = express();
 require("dotenv").config();
 const PORT = process.nextTick.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("");
+});
+
 app.listen(PORT, () => console.log(`🌎 ==> API server now on port ${PORT}!`));
