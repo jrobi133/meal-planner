dfbdb16df7a701a2ddfd13d76f8b95525b25c35d
John Robinson <johnarobinson133@gmail.com>
2021-07-20 14:11:28 -0600

    adding sql database and got down the create and read demo function

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [demoName, setDemoName] = useState("");
+  const [demoDescription, setDemoDescription] = useState("");
+  const [demoList, setDemoList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:5000/api/get").then((response) => {
+      setDemoList(response.data);
+    });
+  }, []);
+
+  const submitDemo = () => {
+    Axios.post("http://localhost:5000/api/insert", {
+      demo_name: demoName,
+      demo_description: demoDescription,
+    }).then(() => {
+      alert("successful insert");
+    });
+  };
+
   return (
     <div className="App">
       <h1>Crud Application</h1>
       <div className="form">
         <label>Demo 1:</label>
-        <input type="text" name="demoOne" />
+        <input
+          type="text"
+          name="demoName"
+          onChange={(e) => {
+            setDemoName(e.target.value);
+          }}
+        />
         <label>Demo 2:</label>
-        <input type="text" name="demoTwo" />
+        <input
+          type="text"
+          name="demoDescription"
+          onChange={(e) => {
+            setDemoDescription(e.target.value);
+          }}
+        />
 
-        <button>Submit</button>
+        <button onClick={submitDemo}>Submit</button>
+
+        {demoList.map((val) => {
+          return (
+            <h1>
+              Demo Name: {val.demo_name} | Demo Description:
+              {val.demo_description}
+            </h1>
+          );
+        })}
       </div>
     </div>
   );
--- server/app.js
+++ server/app.js
@@ -1,12 +1,40 @@
 const express = require("express");
-const mongoose = require("mongoose");
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const mysql = require("mysql");
+
 require("dotenv").config();
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Hunter15",
+  database: "crud_db",
+});
+
 const PORT = process.nextTick.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/get", (req, res) => {
+  const sqlSelect = "SELECT * FROM demo_one ";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+});
+
+app.post("/api/insert", (req, res) => {
+  const demo_name = req.body.demo_name;
+  const demo_description = req.body.demo_description;
+
+  const sqlInsert =
+    "INSERT INTO demo_one (demo_name, demo_description) Values (?,?)";
+  db.query(sqlInsert, [demo_name, demo_description], (err, result) => {
+    console.log(result);
+  });
 });
 
 app.listen(PORT, () => console.log(`🌎 ==> API server now on port ${PORT}!`));
